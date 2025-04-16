1599c9035b73bdbe83028d916613d78ed7e42cd7
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-16 14:11:13 +0600

    added form action

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
 
 function App() {
 
   return (
     <>
       <h1>Explore React Form</h1>
+      <SimpleForm />
     </>
   )
 }
--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const SimpleForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target.name.value)
+    }
     return (
         <div>
-
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="" placeholder='your name' /> <br />
+                <input type="email" placeholder='your email' name="email" id="" />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
