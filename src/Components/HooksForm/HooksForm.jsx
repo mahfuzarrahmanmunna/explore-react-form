b7e0f3dfc98535d8350abd70d2eec11c5035a0d8
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-16 17:55:56 +0600

    added and solve hokksform files

--- src/Components/HooksForm/HooksForm.jsx
+++ src/Components/HooksForm/HooksForm.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import useInputField from '../../Hooks/useInputField';
 
 const HooksForm = () => {
+
+    const [name, nameOnChange] = useInputField('');
+    const [email, emailOnChange] = useInputField('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Submitted', name, email)
+    }
     return (
         <div>
-
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" onChange={nameOnChange} defaultValue={name} placeholder='Name' /><br />
+                <input type="email" name="email" onChange={emailOnChange} defaultValue={email} placeholder='Email' /><br />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
