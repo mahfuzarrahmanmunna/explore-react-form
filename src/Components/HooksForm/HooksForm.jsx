c2c304c7942ff41a01228aaf66f2555fe35b81b3
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-16 18:09:12 +0600

    added productmanagement files

--- src/Components/HooksForm/HooksForm.jsx
+++ src/Components/HooksForm/HooksForm.jsx
@@ -5,16 +5,18 @@ const HooksForm = () => {
 
     const [name, nameOnChange] = useInputField('');
     const [email, emailOnChange] = useInputField('')
+    const [password, passwordOnChange] = useInputField('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('Submitted', name, email)
+        console.log('Submitted', name, email, password)
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="text" name="name" onChange={nameOnChange} defaultValue={name} placeholder='Name' /><br />
                 <input type="email" name="email" onChange={emailOnChange} defaultValue={email} placeholder='Email' /><br />
+                <input type="password" name="password" onChange={passwordOnChange} defaultValue={password} placeholder='Password' /><br />
                 <input type="submit" value="Submit" />
             </form>
         </div>
